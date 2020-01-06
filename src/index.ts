29fb1d50b5c9c50333002c0555d34be2a2fa142f
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-01-06 14:39:51 +0200

    Refactored command class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Command } from '@oclif/command';
 import * as plantumlEncoder from 'plantuml-encoder';
 import { resolve, dirname } from 'path';
-import { createConnection, EntityMetadata } from 'typeorm';
+import { createConnection, EntityMetadata, Connection } from 'typeorm';
 import { ColumnMetadata } from 'typeorm/metadata/ColumnMetadata';
+import { ForeignKeyMetadata } from 'typeorm/metadata/ForeignKeyMetadata';
 
 class TypeormUmlCommand extends Command {
 
@@ -16,47 +17,107 @@ class TypeormUmlCommand extends Command {
 		},
 	];
 
-	async run(): Promise<any> {
+	/**
+	 * Executes this command.
+	 * 
+	 * @async
+	 */
+	public async run(): Promise<any> {
 		const { args } = this.parse( TypeormUmlCommand );
 
 		const configPath = resolve( process.cwd(), args.ormconfig );
 		process.chdir( dirname( configPath ) );
 
-		const connection = await createConnection( require( configPath ) );
-
-		let uml = `@startuml\n`;
-
-		connection.entityMetadatas.forEach( ( entity: EntityMetadata ) => {
-			uml += `\nclass ${ entity.tableNameWithoutPrefix } {\n`;
-
-			entity.columns.forEach( ( column: ColumnMetadata ) => {
-				let prefix = '';
-				if ( column.isPrimary ) {
-					prefix = '+';
-				} else if ( Array.isArray( entity.indices ) && entity.indices.length > 0 ) {
-					const index = entity.indices.find( ( idx ) => idx.columns.map( column => column.databaseName ).includes( column.databaseName ) );
-					if ( index ) {
-						prefix = index.isUnique ? '~' : '#';
-					}
-				}
-
-				uml += `    {method} ${ prefix }${ column.databaseName }: ${ connection.driver.normalizeType( column ) }${ column.length ? `(${ column.length })` : '' }\n`;
-			} );
-
-			uml += `}\n\n`;
-
-			entity.foreignKeys.forEach( ( foreignKey ) => {
-				uml += `${ entity.tableNameWithoutPrefix } "\*" --> "1" ${ foreignKey.referencedTablePath }\n\n`;
-			} );
-		} );
-
-		uml += `@enduml\n`;
+		const url = await this.getUrl( configPath );
 
 		// https://github.com/typeorm/typeorm/blob/master/src/schema-builder/RdbmsSchemaBuilder.ts
 
-		process.stdout.write( `http://www.plantuml.com/plantuml/png/${ encodeURIComponent( plantumlEncoder.encode( uml ) ) }\n` );
+		process.stdout.write( `${ url }\n` );
+	}
+
+	/**
+	 * Builds a plantuml URL and returns it.
+	 * 
+	 * @async
+	 * @param {string} configPath A path to Typeorm config file.
+	 * @returns {string} A plantuml string.
+	 */
+	private async getUrl( configPath: string ): Promise<string> {
+		const connection = await createConnection( require( configPath ) );
+		const uml = this.buildUml( connection );
+		const encodedUml = plantumlEncoder.encode( uml );
 
 		connection.close();
+
+		return `http://www.plantuml.com/plantuml/png/${ encodeURIComponent( encodedUml ) }`;
+	}
+
+	private buildUml( connection: Connection ): string {
+		let uml = `@startuml\n`;
+
+		for ( let i = 0, len = connection.entityMetadatas.length; i < len; i++ ) {
+			uml += this.buildClass( connection.entityMetadatas[i], connection );
+		}
+
+		uml += `@enduml\n`;
+
+		return uml;
+	}
+
+	/**
+	 * Builds an uml class for an entity and returns it.
+	 * 
+	 * @param {EntityMetadata} entity An entity metadata.
+	 * @param {Connection} connection A database connection.
+	 * @returns {string} An uml class string.
+	 */
+	private buildClass( entity: EntityMetadata, connection: Connection ): string {
+		let uml = `\nclass ${ entity.tableNameWithoutPrefix } {\n`;
+
+		for ( let i = 0, len = entity.columns.length; i < len; i++ ) {
+			uml += this.buildColumn( entity.columns[i], entity, connection );
+		}
+
+		uml += `}\n\n`;
+
+		for ( let i = 0, len = entity.foreignKeys.length; i < len; i++ ) {
+			uml += this.buildForeignKeys( entity.foreignKeys[i], entity );
+		}
+
+		return uml;
+	}
+
+	/**
+	 * Builds an uml column and returns it.
+	 * 
+	 * @param {ColumnMetadata} column A column metadata.
+	 * @param {EntityMetadata} entity An entity metadata.
+	 * @param {Connection} connection A database connection.
+	 * @returns {string} An uml column string.
+	 */
+	private buildColumn( column: ColumnMetadata, entity: EntityMetadata, connection: Connection ): string {
+		let prefix = '';
+		if ( column.isPrimary ) {
+			prefix = '+';
+		} else if ( Array.isArray( entity.indices ) && entity.indices.length > 0 ) {
+			const index = entity.indices.find( ( idx ) => idx.columns.map( column => column.databaseName ).includes( column.databaseName ) );
+			if ( index ) {
+				prefix = index.isUnique ? '~' : '#';
+			}
+		}
+
+		return `    {method} ${ prefix }${ column.databaseName }: ${ connection.driver.normalizeType( column ) }${ column.length ? `(${ column.length })` : '' }\n`;
+	}
+
+	/**
+	 * Builds am uml connection string and returns it.
+	 * 
+	 * @param {ForeignKeyMetadata} foreignKey A foreign key metadata.
+	 * @param {EntityMetadata} entity An entity metadata.
+	 * @returns {string} An uml connection string.
+	 */
+	private buildForeignKeys( foreignKey: ForeignKeyMetadata, entity: EntityMetadata ): string {
+		return `${ entity.tableNameWithoutPrefix } "\*" --> "1" ${ foreignKey.referencedTablePath }\n\n`;
 	}
 
 }
