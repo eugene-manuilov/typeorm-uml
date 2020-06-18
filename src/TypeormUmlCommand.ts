14f2e00e085ca02e4641912cd14d1385115a0065
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-06-18 09:19:56 +0300

    Refactored command class, moved uml build command to the separate class

--- src/TypeormUmlCommand.ts
+++ src/TypeormUmlCommand.ts
@@ -4,24 +4,10 @@ import { get } from 'http';
 
 import { Command, flags } from '@oclif/command';
 import * as plantumlEncoder from 'plantuml-encoder';
-import { createConnection, EntityMetadata, Connection, ConnectionOptionsReader } from 'typeorm';
-import { ColumnMetadata } from 'typeorm/metadata/ColumnMetadata';
-import { ForeignKeyMetadata } from 'typeorm/metadata/ForeignKeyMetadata';
+import { createConnection, ConnectionOptionsReader } from 'typeorm';
 
-interface ColumnDataTypeDefaults {
-	length?: string,
-	width?: number,
-	precision?: number,
-	scale?: number,
-}
-
-interface TypeormUmlCommandFlags {
-	format?: string,
-	monochrome?: boolean,
-	connection?: string,
-	include?: string,
-	exclude?: string,
-}
+import { UmlBuilder } from './UmlBuilder';
+import { TypeormUmlCommandFlags } from './TypeormUmlCommandFlags';
 
 class TypeormUmlCommand extends Command {
 
@@ -66,6 +52,8 @@ class TypeormUmlCommand extends Command {
 		} ),
 	};
 
+	protected readonly builder = new UmlBuilder();
+
 	/**
 	 * Executes this command.
 	 * 
@@ -104,7 +92,7 @@ class TypeormUmlCommand extends Command {
 		const connectionOptions = await connectionOptionsReader.get( flags.connection );
 		const connection = await createConnection( connectionOptions );
 
-		const uml = this.buildUml( connection, flags );
+		const uml = this.builder.buildUml( connection, flags );
 		const encodedUml = plantumlEncoder.encode( uml );
 
 		connection.close();
@@ -132,148 +120,6 @@ class TypeormUmlCommand extends Command {
 				response.on( 'end', resolve );
 			} );
 		} );
-	}
-
-	/**
-	 * Builds database uml and returns it.
-	 *
-	 * @private
-	 * @param {Connection} connection A database connection.
-	 * @param {TypeormUmlCommandFlags} flags An object with command flags.
-	 * @returns {string} An uml string.
-	 */
-	private buildUml( connection: Connection, flags: TypeormUmlCommandFlags ): string {
-		let uml = `@startuml\n\n`;
-
-		uml += `!define table(x) class x << (T,#FFAAAA) >>\n`;
-		uml += `!define pkey(x) <b>x</b>\n`;
-		uml += `hide stereotypes\n`;
-		uml += `hide fields\n\n`;
-
-		if ( flags.monochrome ) {
-			uml += `skinparam monochrome true\n\n`;
-		}
-
-		const exclude = ( flags.exclude || '' ).split( ',' ).filter( ( item ) => item.trim().length );
-		const include = ( flags.include || '' ).split( ',' ).filter( ( item ) => item.trim().length );
-
-		for ( let i = 0, len = connection.entityMetadatas.length; i < len; i++ ) {
-			const entity = connection.entityMetadatas[i];
-
-			if ( exclude.includes( entity.name ) ) {
-				continue;
-			}
-
-			if ( include.length && ! include.includes( entity.name ) ) {
-				continue;
-			}
-			
-			uml += this.buildClass( entity, connection );
-		}
-
-		uml += `@enduml\n`;
-
-		return uml;
-	}
-
-	/**
-	 * Builds an uml class for an entity and returns it.
-	 *
-	 * @private
-	 * @param {EntityMetadata} entity An entity metadata.
-	 * @param {Connection} connection A database connection.
-	 * @returns {string} An uml class string.
-	 */
-	private buildClass( entity: EntityMetadata, connection: Connection ): string {
-		let uml = `\ntable( ${ entity.tableNameWithoutPrefix } ) {\n`;
-
-		for ( let i = 0, len = entity.columns.length; i < len; i++ ) {
-			uml += this.buildColumn( entity.columns[i], entity, connection );
-		}
-
-		uml += `}\n\n`;
-
-		for ( let i = 0, len = entity.foreignKeys.length; i < len; i++ ) {
-			uml += this.buildForeignKeys( entity.foreignKeys[i], entity );
-		}
-
-		return uml;
-	}
-
-	/**
-	 * Builds an uml column and returns it.
-	 *
-	 * @private
-	 * @param {ColumnMetadata} column A column metadata.
-	 * @param {EntityMetadata} entity An entity metadata.
-	 * @param {Connection} connection A database connection.
-	 * @returns {string} An uml column string.
-	 */
-	private buildColumn( column: ColumnMetadata, entity: EntityMetadata, connection: Connection ): string {
-		let columnName = column.databaseName;
-		let prefix = '';
-
-		if ( column.isPrimary ) {
-			prefix = '+';
-			columnName = `pkey( ${ columnName } )`;
-		} else if ( Array.isArray( entity.indices ) && entity.indices.length > 0 ) {
-			const index = entity.indices.find( ( idx ) => idx.columns.map( column => column.databaseName ).includes( column.databaseName ) );
-			if ( index ) {
-				prefix = index.isUnique ? '~' : '#';
-			}
-		}
-
-		let length = this.getColumnLength( column );
-		const type = connection.driver.normalizeType( column );
-
-		if ( ! length && connection.driver.dataTypeDefaults[type] ) {
-			length = this.getColumnLength( ( connection.driver.dataTypeDefaults[type] as unknown ) as ColumnDataTypeDefaults );
-		}
-
-		if ( length ) {
-			length = `(${ length })`;
-		}
-
-		return `\t{method} ${ prefix }${ columnName }: ${ type.toUpperCase() }${ length }\n`;
-	}
-
-	/**
-	 * Builds am uml connection string and returns it.
-	 *
-	 * @private
-	 * @param {ForeignKeyMetadata} foreignKey A foreign key metadata.
-	 * @param {EntityMetadata} entity An entity metadata.
-	 * @returns {string} An uml connection string.
-	 */
-	private buildForeignKeys( foreignKey: ForeignKeyMetadata, entity: EntityMetadata ): string {
-		return `${ entity.tableNameWithoutPrefix } "\*" --> "1" ${ foreignKey.referencedTablePath }\n\n`;
-	}
-
-	/**
-	 * Returns a column size or default size if not provided.
-	 *
-	 * @private
-	 * @param {ColumnMetadata | ColumnDataTypeDefaults} column The column instance or data type defaults.
-	 * @returns {string} The column size on success, otherwise empty string.
-	 */
-	private getColumnLength( column: ColumnMetadata | ColumnDataTypeDefaults ): string {
-		if ( column.length ) {
-			return column.length;
-		}
-
-		if ( column.width ) {
-			return column.width.toString();
-		}
-
-		if ( column.precision ) {
-			if ( column.scale ) {
-				return `${ column.precision }, ${ column.scale }`;
-			}
-
-			return column.precision.toString();
-		}
-
-		return '';
 	}
 
 }
---
+++ src/UmlBuilder.ts
@@ -0,0 +1,158 @@
+import { EntityMetadata, Connection } from 'typeorm';
+import { ColumnMetadata } from 'typeorm/metadata/ColumnMetadata';
+import { ForeignKeyMetadata } from 'typeorm/metadata/ForeignKeyMetadata';
+
+import { TypeormUmlCommandFlags } from './TypeormUmlCommandFlags';
+
+interface ColumnDataTypeDefaults {
+	length?: string,
+	width?: number,
+	precision?: number,
+	scale?: number,
+}
+
+export class UmlBuilder {
+
+	/**
+	 * Builds database uml and returns it.
+	 *
+	 * @public
+	 * @param {Connection} connection A database connection.
+	 * @param {TypeormUmlCommandFlags} flags An object with command flags.
+	 * @returns {string} An uml string.
+	 */
+	public buildUml( connection: Connection, flags: TypeormUmlCommandFlags ): string {
+		let uml = `@startuml\n\n`;
+
+		uml += `!define table(x) class x << (T,#FFAAAA) >>\n`;
+		uml += `!define pkey(x) <b>x</b>\n`;
+		uml += `hide stereotypes\n`;
+		uml += `hide fields\n\n`;
+
+		if ( flags.monochrome ) {
+			uml += `skinparam monochrome true\n\n`;
+		}
+
+		const exclude = ( flags.exclude || '' ).split( ',' ).filter( ( item ) => item.trim().length );
+		const include = ( flags.include || '' ).split( ',' ).filter( ( item ) => item.trim().length );
+
+		for ( let i = 0, len = connection.entityMetadatas.length; i < len; i++ ) {
+			const entity = connection.entityMetadatas[i];
+
+			if ( exclude.includes( entity.name ) ) {
+				continue;
+			}
+
+			if ( include.length && ! include.includes( entity.name ) ) {
+				continue;
+			}
+			
+			uml += this.buildClass( entity, connection );
+		}
+
+		uml += `@enduml\n`;
+
+		return uml;
+	}
+
+	/**
+	 * Builds an uml class for an entity and returns it.
+	 *
+	 * @protected
+	 * @param {EntityMetadata} entity An entity metadata.
+	 * @param {Connection} connection A database connection.
+	 * @returns {string} An uml class string.
+	 */
+	protected buildClass( entity: EntityMetadata, connection: Connection ): string {
+		let uml = `\ntable( ${ entity.tableNameWithoutPrefix } ) {\n`;
+
+		for ( let i = 0, len = entity.columns.length; i < len; i++ ) {
+			uml += this.buildColumn( entity.columns[i], entity, connection );
+		}
+
+		uml += `}\n\n`;
+
+		for ( let i = 0, len = entity.foreignKeys.length; i < len; i++ ) {
+			uml += this.buildForeignKeys( entity.foreignKeys[i], entity );
+		}
+
+		return uml;
+	}
+
+	/**
+	 * Builds an uml column and returns it.
+	 *
+	 * @protected
+	 * @param {ColumnMetadata} column A column metadata.
+	 * @param {EntityMetadata} entity An entity metadata.
+	 * @param {Connection} connection A database connection.
+	 * @returns {string} An uml column string.
+	 */
+	protected buildColumn( column: ColumnMetadata, entity: EntityMetadata, connection: Connection ): string {
+		let columnName = column.databaseName;
+		let prefix = '';
+
+		if ( column.isPrimary ) {
+			prefix = '+';
+			columnName = `pkey( ${ columnName } )`;
+		} else if ( Array.isArray( entity.indices ) && entity.indices.length > 0 ) {
+			const index = entity.indices.find( ( idx ) => idx.columns.map( column => column.databaseName ).includes( column.databaseName ) );
+			if ( index ) {
+				prefix = index.isUnique ? '~' : '#';
+			}
+		}
+
+		let length = this.getColumnLength( column );
+		const type = connection.driver.normalizeType( column );
+
+		if ( ! length && connection.driver.dataTypeDefaults[type] ) {
+			length = this.getColumnLength( ( connection.driver.dataTypeDefaults[type] as unknown ) as ColumnDataTypeDefaults );
+		}
+
+		if ( length ) {
+			length = `(${ length })`;
+		}
+
+		return `\t{method} ${ prefix }${ columnName }: ${ type.toUpperCase() }${ length }\n`;
+	}
+
+	/**
+	 * Builds am uml connection string and returns it.
+	 *
+	 * @protected
+	 * @param {ForeignKeyMetadata} foreignKey A foreign key metadata.
+	 * @param {EntityMetadata} entity An entity metadata.
+	 * @returns {string} An uml connection string.
+	 */
+	protected buildForeignKeys( foreignKey: ForeignKeyMetadata, entity: EntityMetadata ): string {
+		return `${ entity.tableNameWithoutPrefix } "\*" --> "1" ${ foreignKey.referencedTablePath }\n\n`;
+	}
+
+	/**
+	 * Returns a column size or default size if not provided.
+	 *
+	 * @protected
+	 * @param {ColumnMetadata | ColumnDataTypeDefaults} column The column instance or data type defaults.
+	 * @returns {string} The column size on success, otherwise empty string.
+	 */
+	protected getColumnLength( column: ColumnMetadata | ColumnDataTypeDefaults ): string {
+		if ( column.length ) {
+			return column.length;
+		}
+
+		if ( column.width ) {
+			return column.width.toString();
+		}
+
+		if ( column.precision ) {
+			if ( column.scale ) {
+				return `${ column.precision }, ${ column.scale }`;
+			}
+
+			return column.precision.toString();
+		}
+
+		return '';
+	}
+
+}
